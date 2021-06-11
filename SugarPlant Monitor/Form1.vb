35869ac3aaa9c5d45cd56d1c76497ca8219d3ab3
Robert Moore <moorer2k@gmail.com>
2021-06-11 15:59:45 -0700

    Update Form1.vb
    Added thread safety and fixed the URI endpoint.

--- SugarPlant Monitor/Form1.vb
+++ SugarPlant Monitor/Form1.vb
@@ -1,4 +1,6 @@
-﻿Imports Flurl.Http
+Imports System.IO
+Imports System.Net
+Imports Flurl.Http
 Imports Newtonsoft.Json
 
 Public Class Form1
@@ -6,12 +8,28 @@ Public Class Form1
     Private Property SClient As New FlurlClient
 
     Private Async Function GetData(wallet As String, Optional pass As String = "nopass") As Task(Of PoolData)
+
         Try
-            Dim url As String = "https://pool.rplant.xyz/api/poolminer2/sugarchain/" & wallet
-            Dim resp = Await url.WithClient(SClient).PostJsonAsync(New With {Key .password = pass})
-            Dim strResp = Await resp.GetStringAsync()
-            Dim jResp = JsonConvert.DeserializeObject(Of PoolData)(strResp)
-            Return jResp
+            Dim encoded = Base64Encode($"{wallet}|{pass}")
+
+            Dim url As String = "https://pool.rplant.xyz/api2/poolminer2/sugarchain/" & wallet & "/" & encoded
+            Dim str = Await url.WithClient(SClient).GetStreamAsync(completionOption:=Http.HttpCompletionOption.ResponseHeadersRead)
+
+            Using reader = New StreamReader(str)
+
+                Do While Not reader.EndOfStream
+                    Dim sbdata = reader.ReadLine()
+                    If sbdata <> "" Then
+                        Dim sbjson = sbdata.Substring(6)
+                        If sbjson.StartsWith("{" & Chr(34) & "miner") Then
+                            Dim pData = JsonConvert.DeserializeObject(Of PoolData)(sbjson)
+                            Return pData
+                        End If
+                    End If
+
+                Loop
+
+            End Using
 
         Catch ex As Exception
 
@@ -20,36 +38,44 @@ Public Class Form1
         Return Nothing
 
     End Function
+    Public Shared Function Base64Encode(ByVal plainText As String) As String
+        Dim plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText)
+        Return System.Convert.ToBase64String(plainTextBytes)
+    End Function
 
     Private Async Function ButtonCheck_ClickAsync(sender As Object, e As EventArgs) As Task Handles ButtonCheck.Click
 
         FlatStatusBar1.Text = "Status: Retrieving pool stats..."
 
-        Dim data = Await GetData(TextWallet.Text)
+        Dim threadGet As New Threading.Thread(Async Sub()
 
-        If data IsNot Nothing Then
-            LabelWorkers.Text = data.Miner.Wc
-            LabelPaid.Text = data.Miner.Paid
-            LabelHashrate.Text = data.Miner.Hr
-            LabelCurrBalance.Text = data.Miner.Balance
-            FlatStatusBar1.Text = "Status: Stats recieved!"
+                                                  Dim data = Await GetData(TextWallet.Text)
 
-        Else
-            FlatStatusBar1.Text = "Status: Failed to retrieve stats."
-        End If
+                                                  If data IsNot Nothing Then
+                                                      SetText(Me, LabelWorkers, data.Miner.Wc)
+                                                      SetText(Me, LabelPaid, data.Miner.Paid)
+                                                      SetText(Me, LabelHashrate, data.Miner.Hr)
+                                                      SetText(Me, LabelCurrBalance, data.Miner.Balance)
+                                                      SetText(Me, FlatStatusBar1, "Status: Stats recieved!")
+                                                  Else
+                                                      SetText(Me, FlatStatusBar1, "Status: Failed to retrieve stats.")
+                                                  End If
+                                              End Sub)
+
+        threadGet.Start()
 
     End Function
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
-    
+
+        ServicePointManager.Expect100Continue = True
         TextWallet.Text = My.Settings.Wallet
         TextPass.Text = My.Settings.Password
 
         Dim headers As New Dictionary(Of String, String) From {
             {"Connection", "keep-alive"},
-            {"Accept", "application/json, text/javascript, */*; q=0.01"},
-            {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"},
-            {"Content-Type", "application/json"}
+            {"Accept", "text/event-stream"},
+            {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"}
         }
 
         SClient.WithHeaders(headers)
@@ -60,6 +86,16 @@ Public Class Form1
         My.Settings.Wallet = TextWallet.Text
         My.Settings.Password = TextPass.Text
         My.Settings.Save()
+    End Sub
+
+    Delegate Sub SetText_Delegate(Of T As {New, Control})([Form] As Form, [Control] As T, [Text] As String)
+    Public Sub SetText(Of T As {New, Control})([Form] As Form, [Control] As T, [Text] As String)
+        If [Control].InvokeRequired Then
+            Dim MyDelegate As New SetText_Delegate(Of T)(AddressOf SetText)
+            [Form].Invoke(MyDelegate, New Object() {[Form], [Control], [Text]})
+        Else
+            [Control].Text = [Text]
+        End If
     End Sub
 
 End Class
